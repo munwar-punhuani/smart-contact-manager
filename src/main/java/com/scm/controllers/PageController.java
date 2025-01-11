abedf762a47efdf2b350e46c1ab285174ce5ec32
Munwar Punhuani <munwar.punhuani@outlook.com>
2025-01-11 22:50:36 +0500

    Enhance PageController to pass dynamic data to home page view

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,18 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("Home Page Controller");
+        model.addAttribute("firstName","Munwar Punhuani");
+        model.addAttribute("occupation","Software Engineer");
+        model.addAttribute("address", "Karachi");
         return "home";
     }
 
