51de3a5405c44d0dbf6cee0ced1a8658dd5f8148
Munwar Punhuani <munwar.punhuani@outlook.com>
2025-01-20 21:47:51 +0500

    Add navbar with links to contact, login, and signup pages

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -2,33 +2,52 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(Model model){
+    public String home(Model model) {
         System.out.println("Home Page Controller");
-        model.addAttribute("firstName","Munwar Punhuani");
-        model.addAttribute("occupation","Software Engineer");
+        model.addAttribute("firstName", "Munwar Punhuani");
+        model.addAttribute("occupation", "Software Engineer");
         model.addAttribute("address", "Karachi");
         return "home";
     }
 
     // about route
     @RequestMapping("/about")
-    public String aboutPage(Model model){
+    public String aboutPage(Model model) {
         System.out.println("About Page Loading");
-        model.addAttribute("isLogin",false);
+        model.addAttribute("isLogin", false);
         return "about";
     }
 
-    //services route
+    // services route
     @RequestMapping("services")
-    public String servicesPage(){
+    public String servicesPage() {
         System.out.println("Services Page Loading");
         return "services";
+    }
+
+    // Contact us route
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    // login route
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // signup route
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,59 @@
+console.log("Script loaded");
+
+let currentTheme = getTheme();
+
+//initial
+document.addEventListener("DOMContentLoaded", () => {
+  changeTheme();
+});
+
+//TODO:
+function changeTheme() {
+  changePageTheme(currentTheme, currentTheme);
+
+  //set the listener  to change theme button
+  const changeThemeButton = document.querySelector("#theme_change_button");
+
+  //   changeThemeButton.querySelector("span").textContent =
+  //     currentTheme == "light" ? "Dark" : "Light";
+
+  const oldTheme = currentTheme;
+  changeThemeButton.addEventListener("click", (event) => {
+    console.log("change theme button clicked");
+
+    if (currentTheme == "dark") {
+      // theme to light
+      currentTheme = "light";
+    } else {
+      // theme to dark
+      currentTheme = "dark";
+    }
+    changePageTheme(currentTheme, oldTheme);
+  });
+}
+
+//set theme to localStorage
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+//get theme to localStorage
+function getTheme() {
+  let theme = localStorage.getItem("theme");
+  console.log("inside get theme method: " + theme);
+  return theme ? theme : "light";
+}
+
+// change current page theme
+function changePageTheme(theme, oldTheme) {
+  // update current theme into local Storage
+  setTheme(currentTheme);
+  // remove the current theme
+  document.querySelector("html").classList.remove(oldTheme);
+  // set the current theme
+  document.querySelector("html").classList.add(theme);
+  // change the text of button
+  document
+    .querySelector("#theme_change_button")
+    .querySelector("span").textContent = theme == "light" ? "Dark" : "Light";
+}
