d10066869904b7d719b2fcdcd0dc3fed8fecd672
Munwar Punhuani <munwar.punhuani@outlook.com>
2025-01-25 22:57:59 +0500

    Add Hibernate Validator for user input validation and display messages on frontend

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.scm.entities.User;
 import com.scm.forms.UserForm;
 import com.scm.helpers.Message;
-import com.scm.helpers.Message.MessageBuilder;
 import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
@@ -76,11 +77,14 @@ public class PageController {
 
     // Processing register
     @RequestMapping(value = "do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm,HttpSession session) {
+    public String processRegister(@Valid @ModelAttribute UserForm userForm,BindingResult bindingResult ,HttpSession session) {
         System.out.println("Processing Registration");
         // fetch form data
         System.out.println(userForm);
         // validate form data
+        if(bindingResult.hasErrors()){
+            return "register"; 
+        }
         // TODO: NEXT VIDEO
         // save to database
 
